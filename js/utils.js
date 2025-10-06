9185737ce0b548e800852c6a21cf940d9690e191
tb59 <thomas.bourdeaudhuy@gmail.com>
2025-10-06 08:59:19 +0200

    correction de tryit, fourniture des fonctions utils.js et boucle.js

--- js/utils.js
+++ js/utils.js
@@ -1,93 +1,53 @@
-// Bibliothèque de fonctions utilitaires
-
-function trace(s) {
-  // que fait cette fonction ?
-  window.console && console.log(s);
-  
-  /*
-  // Équivalent à :
-  if (window.console) {
-    console.log(s);
-  }
-  */
-}
-
-function mkDebug() {
-  // Crée des fonctions debug
-  
-  // Variable locale à mkDebug
-  var compteur = 10;
-
-  // Fonction anonyme (sans nom)
-  return function(s) {
-    // affiche un nombre de messages limité par un compteur
-    if (compteur > 0) {
-      if (s === undefined) {
-        trace(compteur);
-      } else {
-        trace(s);
-      }
-      compteur--;
-    }
-  }
-}
-
-var debug = mkDebug();
-
-function getRef(refOrId) {
-  // Retourne la référence d'un élément dont la référence ou l'identifiant est fourni
-  var ref;
-  if (typeof refOrId === "string") {
-    ref = document.getElementById(refOrId);
-  } else {
-    ref = refOrId;
-  }
-  return ref;
-}
-
-function show(refOrId, display = "block") {
-  // affiche l'élément dont la référence ou l'id est fourni
-  // le paramètre display doit valoir block par défaut
-  var ref = getRef(refOrId);
-  
-  /*
-  // Valeur par défaut à la main
-  if (display === undefined) {
-    display = "block";
-  }
-  */
-  
-  ref.style.display = display;
+function show(refOrId,display) {
+	// affiche l'élément dont la référence ou l'id est fourni
+	// le paramètre display doit valoir block par défaut
+	// cf. ref.style.display = "none"|"block"
+	if (typeof refOrId == "string") 
+		refOrId = document.getElementById(refOrId); 
+	if (display == undefined) display = "block";
+	
+	refOrId.style.display = display; 
 }
 
 function hide(refOrId) {
-  // cache l'élément dont la référence ou l'id est fourni
-  show(refOrId, "none");
-}
-
-function getOrReplaceContent(ref, prop, val) {
-  // Retourne le contenu d'un élément ou remplace son contenu si val est fourni
-  if (val === undefined) {
-    return ref[prop];
-  } else {
-    ref[prop] = val;
-  }
+	// cache l'élément dont la référence ou l'id est fourni
+	if (typeof refOrId == "string") 
+		refOrId = document.getElementById(refOrId); 
+		
+		refOrId.style.display = "none";
 }
 
 function html(refOrId, val) {
-  // affecte une valeur à l'élément dont la référence ou l'id est fourni; si val n'est pas fourni, on renvoie son contenu
-  var ref = getRef(refOrId);
-  return getOrReplaceContent(ref, "innerHTML", val);
+	// affecte une valeur à l'élément dont la référence ou l'id est fourni; si val n'est pas fourni, on renvoie son contenu
+	if (typeof refOrId == "string") 
+		refOrId = document.getElementById(refOrId);
+	
+	if (val == undefined) return refOrId.innerHTML; 
+	else {
+		refOrId.innerHTML = val ;
+	}
 }
 
 function val(refOrId, val) {
-  // affecte une valeur à l'élément dont la référence ou l'id est fourni; si val n'est pas fourni, on renvoie son contenu
-  // l'élément est un champ de formulaire
-  // la fonction doit pouvoir manipuler l'état des champs de type checkbox et radio 
-  var ref = getRef(refOrId);
-  if (ref.type === "checkbox" || ref.type === "radio") {
-    return getOrReplaceContent(ref, "checked", val);
-  } else {
-    return getOrReplaceContent(ref, "value", val);
-  }
+	// affecte une valeur à l'élément dont la référence ou l'id est fourni; si val n'est pas fourni, on renvoie son contenu
+	// l'élément est un champ de formulaire
+	// la fonction doit pouvoir manipuler l'état des champs de type checkbox et radio 
+	if (typeof refOrId == "string") 
+		refOrId = document.getElementById(refOrId);
+	if ( (refOrId.type == "checkbox") 
+			|| (refOrId.type == "radio") )
+	{
+		if (val == undefined) return refOrId.checked; 
+		else {
+			refOrId.checked = val ;
+		}
+	} else {
+		if (val == undefined) return refOrId.value; 
+		else {
+			refOrId.value = val ;
+		}
+	}
 }
+
+console.log("Chargement de utils.js : html, val, show, hide  ");
+
---
+++ js/utils_max.js
@@ -0,0 +1,93 @@
+// Bibliothèque de fonctions utilitaires
+
+function trace(s) {
+  // que fait cette fonction ?
+  window.console && console.log(s);
+  
+  /*
+  // Équivalent à :
+  if (window.console) {
+    console.log(s);
+  }
+  */
+}
+
+function mkDebug() {
+  // Crée des fonctions debug
+  
+  // Variable locale à mkDebug
+  var compteur = 10;
+
+  // Fonction anonyme (sans nom)
+  return function(s) {
+    // affiche un nombre de messages limité par un compteur
+    if (compteur > 0) {
+      if (s === undefined) {
+        trace(compteur);
+      } else {
+        trace(s);
+      }
+      compteur--;
+    }
+  }
+}
+
+var debug = mkDebug();
+
+function getRef(refOrId) {
+  // Retourne la référence d'un élément dont la référence ou l'identifiant est fourni
+  var ref;
+  if (typeof refOrId === "string") {
+    ref = document.getElementById(refOrId);
+  } else {
+    ref = refOrId;
+  }
+  return ref;
+}
+
+function show(refOrId, display = "block") {
+  // affiche l'élément dont la référence ou l'id est fourni
+  // le paramètre display doit valoir block par défaut
+  var ref = getRef(refOrId);
+  
+  /*
+  // Valeur par défaut à la main
+  if (display === undefined) {
+    display = "block";
+  }
+  */
+  
+  ref.style.display = display;
+}
+
+function hide(refOrId) {
+  // cache l'élément dont la référence ou l'id est fourni
+  show(refOrId, "none");
+}
+
+function getOrReplaceContent(ref, prop, val) {
+  // Retourne le contenu d'un élément ou remplace son contenu si val est fourni
+  if (val === undefined) {
+    return ref[prop];
+  } else {
+    ref[prop] = val;
+  }
+}
+
+function html(refOrId, val) {
+  // affecte une valeur à l'élément dont la référence ou l'id est fourni; si val n'est pas fourni, on renvoie son contenu
+  var ref = getRef(refOrId);
+  return getOrReplaceContent(ref, "innerHTML", val);
+}
+
+function val(refOrId, val) {
+  // affecte une valeur à l'élément dont la référence ou l'id est fourni; si val n'est pas fourni, on renvoie son contenu
+  // l'élément est un champ de formulaire
+  // la fonction doit pouvoir manipuler l'état des champs de type checkbox et radio 
+  var ref = getRef(refOrId);
+  if (ref.type === "checkbox" || ref.type === "radio") {
+    return getOrReplaceContent(ref, "checked", val);
+  } else {
+    return getOrReplaceContent(ref, "value", val);
+  }
+}
