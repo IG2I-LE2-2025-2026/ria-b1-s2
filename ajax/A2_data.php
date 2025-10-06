5a0469ec877d0ba977919ac82bb6e443905a1079
tb59 <thomas.bourdeaudhuy@gmail.com>
2025-10-06 09:59:47 +0200

    correction ajax A2

---
+++ ajax/A2_data.php
@@ -0,0 +1,20 @@
+<?php
+
+$salles = array("Maxime"=>"305","Thomas"=>"005");
+$noms = array("Maxime"=>"Folschette","Thomas"=>"Bourdeaud'huy");
+
+if ((isset($_GET["cle"]) && isset($noms[$_GET["cle"]])))
+{
+	echo $noms[$_GET["cle"]];
+	die("");
+}
+
+if ((isset($_POST["cle"]) && isset($salles[$_POST["cle"]]))) 
+{
+	echo $salles[$_POST["cle"]];
+	die("");
+}
+
+echo "Aucun Resultat";
+
+?>
